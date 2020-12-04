a803c4f8d85317b46a438cbf230ad04ceb7f1af4
Ayajiro <nakamura.ayajiro@gmail.com>
2020-12-04 18:38:01 +0800

    fix build bug

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,92 +1,79 @@
+// setting
 module.exports = {
-  title: "Ayajiro",
-  description: "Web development, Frontend, Javascript, Cat",
-  theme: "@vuepress/theme-blog", // OR shortcut: @vuepress/blog
-  themeConfig: {
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
-     */
-    modifyBlogPluginOptions(blogPluginOptions) {
-      return blogPluginOptions
+    title: "Ayajiro",
+    description: "Web development, Frontend, Javascript",
+    port:"8080",
+    theme: "@vuepress/blog",
+    themeConfig: {
+        nav: [
+            {
+              text: "日文文法指南",
+              link: "/jpgramma_zhtw/"
+            },
+            {
+              text: "日常筆記",
+              link: "/post/"
+            },
+            {
+              text: "GitHub",
+              link: "https://github.com/Ayajiro"
+            },
+            {
+                text: "About",
+                link: "/"
+            }
+            
+          ],
+        directories: [
+            {
+                id:"index",
+                title:"首頁",
+                dirname:"/",
+                path:"",
+            },
+            {
+                id:"post",
+                dirname:"_posts",
+                title:"文章",
+                path:"/post/",
+                itemPermalink: "/:year/:month/:day/:slug"
+            }
+        ],
+        footer: {
+            contact: [
+              {
+                type: "github",
+                link: "https://github.com/Ayajiro"
+              },
+              {
+                type: 'twitter',
+                link: 'https://twitter.com/oscrx78',
+              },
+              {
+                type: "mail",
+                link: "mailto:nakamura.ayajiro@gmail.com"
+              },
+              {
+                type: "youtube",
+                link: "https://www.youtube.com/channel/UCt_FBDYnWZJHE2IDgPPW2LQ"
+              }
+            ],
+            copyright: [
+              {
+                text: "Ayajiro © 2020",
+                link: ""
+              }
+            ]
+          },
+          smoothScroll: true
+          
     },
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
-     */
-    nav: [
-      {
-        text: "日文文法指南",
-        link: "/jpgramma_zhtw/"
-      },
-      {
-        text: "日常筆記",
-        link: "/post/"
-      },
-      {
-        text: "GitHub",
-        link: "https://github.com/Ayajiro"
-      },
-      {
-          text: "About",
-          link: "/"
-      },
-      {
-          // text: "OldBlog",
-          // link: "/oldblog/"
-      }
+    head: [
+        ["link", { rel: "icon", href: "/logo.png" }]
     ],
-    directories: [
-      {
-          id:"index",
-          title:"首頁",
-          dirname:"/",
-          path:"MyIndexPost",
-      },
-      {
-          id:"post",
-          dirname:"_posts",
-          title:"文章",
-          path:"/post/",
-          itemPermalink: "/:year/:month/:day/:slug"
-      }
-    ],
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
-     */
-    footer: {
-      contact: [
-        {
-          type: "github",
-          link: "https://github.com/Ayajiro"
-        },
-        {
-          type: 'twitter',
-          link: 'https://twitter.com/oscrx78',
-        },
-        {
-          type: "mail",
-          link: "mailto:nakamura.ayajiro@gmail.com"
-        },
-        {
-          type: "youtube",
-          link: "https://www.youtube.com/channel/UCt_FBDYnWZJHE2IDgPPW2LQ"
+    locales:{
+        "/":{
+            lang:"zh-TW",
         }
-      ],
-      copyright: [
-        {
-          text: "Ayajiro © 2020",
-          link: ""
-        }
-      ]
-    },
-    smoothScroll: true 
-    },
-  
-  head: [
-    ["link", { rel: "icon", href: "/logo.png" }]
-  ],
-  locales:{
-    "/":{
-        lang:"zh-TW",
     }
-  }
 }
